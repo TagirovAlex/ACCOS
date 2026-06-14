9aba0ae8a0e58a74283c7c24ea810aa5ca40b344
TagirovAlex <tagirov.alex@gmail.com>
2026-06-14 09:44:52 +0300

    [chore] gitignore tmp/ remove tracked tmp files

--- tmp/ls_models.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import json
-
-req = urllib.request.Request("http://10.82.4.99:1234/v1/models")
-resp = json.loads(urllib.request.urlopen(req, timeout=10).read())
-for m in resp.get("data", []):
-    mid = m.get("id", "?")
-    print(mid)
